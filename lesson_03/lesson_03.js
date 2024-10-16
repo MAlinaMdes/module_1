2afb73bb3adbefe623120be95f1e2ba57301b2e4
M_alina <misskazzakova@gmail.com>
2024-10-17 01:03:01 +0300

    задание 1

---
+++ lesson_03/lesson_03.js
@@ -0,0 +1,36 @@
+'use strict'
+
+{
+  const item = 'smart проектор philips';
+  const count = 15;
+  const category = 'видео техника';
+  const price = 1200;
+
+  // console.log(item);
+  // console.log(`общая сумма товара: ${count * price}`);
+}
+
+{
+    const itemSecond = 'серьги';
+    const categorySecond = 'jewellery'
+}
+  /**
+  *   Создайте второй товар
+  *   Другой категории
+  */
+  {
+    const nameOfGood = prompt('Укажите наименование товара', '');
+    const priceOfGood = Number(prompt('цена товара', ''));
+
+    const countOfGood = Number(prompt('Количество товара', ''));
+ 
+    const typeOfGood = prompt('Тип товара', '');
+
+    console.log(typeof(priceOfGood));
+    console.log(typeof(countOfGood));
+  
+
+
+
+  }
+  
