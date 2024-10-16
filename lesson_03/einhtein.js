fe01d6527e83b81af9cf9ac6b5983382fafb32b1
M_alina <misskazzakova@gmail.com>
2024-10-17 01:19:57 +0300

    второе задание выполнено

---
+++ lesson_03/einhtein.js
@@ -0,0 +1,8 @@
+'use strict'
+{
+    const myWeight = 45;
+    const speedLight= 3e8;
+    const energy = (myWeight * (Math.pow(3e8, 2)));
+    console.log(energy);
+    console.log(typeof(speedLight));
+}
