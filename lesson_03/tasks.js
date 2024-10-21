aad09ecebfb4d7e1d9a2c63735260aba9e7570b0
M_alina <misskazzakova@gmail.com>
2024-10-21 23:11:06 +0300

    не понимаю как записать выражение из первой реинн

---
+++ lesson_03/tasks.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let rain = Math.random();
+console.log(rain);
+
+rain = Math.round(1);;
+console.log(rain)
+
+if (rain == 1) {
+    console.log ('Пошёл дождь. Возьмите зонт!')
+} else if (rain == 0) {
+    console.log ('Дождя нет')
+}
