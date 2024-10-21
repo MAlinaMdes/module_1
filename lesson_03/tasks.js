d55570a14b7eed336ec259127ff3423769e538b0
M_alina <misskazzakova@gmail.com>
2024-10-21 23:58:57 +0300

    task 4 done

--- lesson_03/tasks.js
+++ lesson_03/tasks.js
@@ -1,17 +1,17 @@
 'use strict'
 
-// let rain = Math.random();
-// console.log(rain);
+let rain = Math.random();
+console.log(rain);
 
-// rain = Math.round(1);;
-// console.log(rain)
+rain = Math.round(1);;
+console.log(rain)
 
-// if (rain == 1) {
-//     console.log ('Пошёл дождь. Возьмите зонт!')
-// } else if (rain == 0) {
-//     console.log ('Дождя нет')
-// }
-// задача 3
+if (rain == 1) {
+    console.log ('Пошёл дождь. Возьмите зонт!')
+} else if (rain == 0) {
+    console.log ('Дождя нет')
+}
+задача 3
 
 const mathematic = Number(prompt('Введите кол-во баллов по математике', ''));
 const rusLang = Number(prompt('Введите кол-во баллов по русскому языку',''));
@@ -22,3 +22,12 @@ const totalScore = mathematic + rusLang + computerScore;
 if (totalScore >= 265) {
     console.log('Поздравляю, вы поступили на бюджет!')
 }
+task 4 
+
+const amountOfMoney = Number(prompt('Какую суммы вы желаете снять',''));
+if (amountOfMoney % 100 === 0 ) {
+    console.log('сумма доступна')
+} else {
+    console.log('введите сумму кратную 100')
+}
+
